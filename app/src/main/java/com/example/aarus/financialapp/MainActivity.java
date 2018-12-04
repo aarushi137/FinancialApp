bef8fb607a6b7d862ddb32be57d96ea642c6f038
skondi2 <Salem495!>
2018-12-04 13:59:33 -0600

    added the green background on the opening screen

--- app/src/main/java/com/example/aarus/financialapp/MainActivity.java
+++ app/src/main/java/com/example/aarus/financialapp/MainActivity.java
@@ -2,13 +2,9 @@ package com.example.aarus.financialapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
