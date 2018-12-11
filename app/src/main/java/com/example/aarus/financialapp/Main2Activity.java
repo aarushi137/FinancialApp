03df273a0de80dc4bd026cec18321311a079d469
skondi2 <Salem495!>
2018-12-10 22:40:02 -0600

    finished all the bank info pages

--- app/src/main/java/com/example/aarus/financialapp/Main2Activity.java
+++ app/src/main/java/com/example/aarus/financialapp/Main2Activity.java
@@ -4,26 +4,72 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
+
 
 public class Main2Activity extends AppCompatActivity {
-    String[] items = new String[] {"PNC Bank", "Busey Bank", "Marine Bank", "Chase", "Bank Champaign", "U of I Community Credit Union",
-            "First Federal Savings Bank of Champaign-Urbana", "Central Illinios Bank", "Regions Bank"};
+    // to delete
+    //String[] items = new String[] {"PNC Bank", "Busey Bank", "Marine Bank", "Chase", "Bank Champaign", "U of I Community Credit Union",
+            //"First Federal Savings Bank of Champaign-Urbana", "Central Illinois Bank", "Regions Bank"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.bank_list);
+        setContentView(R.layout.bank_buttons);
 
-        ListView listView = (ListView) findViewById(R.id.banksList);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        Button pncButton = (Button) findViewById(R.id.button_pnc);
+        pncButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent startIntent = new Intent(getApplicationContext(), Main3Activity.class);
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), Main22Activity.class);
+                startActivity(startIntent);
+            }
+        });
+        Button buseyButton = (Button) findViewById(R.id.button_busey);
+        buseyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), busey_bank.class);
+                startActivity(startIntent);
+            }
+        });
+        Button marineButton = (Button) findViewById(R.id.button_marine);
+        marineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), marine_bank.class);
+                startActivity(startIntent);
+            }
+        });
+        Button chaseButton = (Button) findViewById(R.id.button_chase);
+        chaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), chase.class);
+                startActivity(startIntent);
+            }
+        });
+        Button champaignButton = (Button) findViewById(R.id.button_chase);
+        champaignButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), bank_champaign.class);
+                startActivity(startIntent);
+            }
+        });
+        Button firstButton = (Button) findViewById(R.id.button_firstfederal);
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), first_federal.class);
+                startActivity(startIntent);
+            }
+        });
+        Button centralButton = (Button) findViewById(R.id.button_central);
+        centralButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), central_illinois_bank.class);
                 startActivity(startIntent);
             }
         });
--- app/src/main/java/com/example/aarus/financialapp/chase.java
+++ app/src/main/java/com/example/aarus/financialapp/chase.java
@@ -5,15 +5,16 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class Main3Activity extends AppCompatActivity {
-    String[] items = new String[] {"Name: ", "Certificate Deposit Rate: ", "ATM fee: ", "Overdraft fee: ", "Minimum balance: ", "Card Replacement Fee: "};
+public class chase extends AppCompatActivity {
+    String[] items = new String[] {"Name : Chase Bank", "Certificate Deposit Rate: 0.90%", "Annual Percentage Yield: 0.90%", "ATM Fee: $2.50", "Overdraft Fee: $34 per item", "Minimum Balance: $25",
+            "Card Replacement Fee: $5.00"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.detail_list);
+        setContentView(R.layout.activity_chase);
 
-        ListView listView = (ListView) findViewById(R.id.detailsList);
+        ListView listView = (ListView) findViewById(R.id.chase_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter(adapter);
     }
